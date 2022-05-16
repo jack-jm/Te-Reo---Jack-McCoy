58eddb4753295ea3a0b22e0ad98e59e408e73962
Jack McCoy <101677953+jack-jm@users.noreply.github.com>
2022-05-16 01:00:10 +0000

    Finished instructions and started working on lists.

--- main.py
+++ main.py
@@ -2,10 +2,17 @@
 show_instructions = input("Kia Ora, have you played the quiz before?\n").lower()
 
 if show_instructions == "yes" or show_instructions == "y":
-  print("Quiz starts")
+  print("Let's begin!")
 
 elif show_instructions == "no" or show_instructions == "n":
-  print("Here are the instructions")
+  print("Instructions:")
+  print("You will be given a word in Te Reo and you will have to match it to it's English meaning out of four options. You will type either A, B, C, or D, depending on what option you think is most likely correct. The words will start easy and will later become more difficult.")
 
 else:
-  print("Please answer yes or no.")
+  print(end="Please answer yes or no.")
+
+score = 0
+#Lists of words based on difficulty
+easy = ["Kia Ora", "Kai", "Aroha", "Iwi", "Marae", "Aotearoa", "Whānau", "Mahi", "Whenua", "Hangi"]
+
+play_again = input("Press <enter> to play.")
