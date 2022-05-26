9961a175d586019e45a97c1e8f83636e4bc52fc4
Jack McCoy <101677953+jack-jm@users.noreply.github.com>
2022-05-26 22:36:13 +0000

    Started component 3 and finished all documentation on component 2. Will need to figure out how to tell the user if they were correct or not as it is not working as of now.

--- main.py
+++ main.py
@@ -8,19 +8,19 @@ if show_instructions == "yes" or show_instructions == "y":
 
 elif show_instructions == "no" or show_instructions == "n":
   print("Instructions:")
-  print("You will be given a word in Te Reo and you will have to match it to it's English meaning out of four options. You will type either A, B, C, or D, depending on what option you think is most likely correct. The words will start easy and will later become more difficult.\nLet's begin!\n")
+  print("You will be given a word in English and you will have to match it to it's Te Reo counterpart out of four options. You will type either A, B, C, or D, depending on what option you think is most likely correct. The words will start easy and will later become more difficult. When you want to stop, type anything other than enter and you will see your final score. \nLet's begin!\n")
 
 else:
-  print("Please answer yes or no.")
-  exit()
+  print("Instructions:")
+  print("You will be given a word in English and you will have to match it to it's Te Reo counterpart out of four options. You will type either A, B, C, or D, depending on what option you think is most likely correct. The words will start easy and will later become more difficult. When you want to stop, type anything other than enter and you will see your final score. \nLet's begin!\n")
 
-# Component 2: Generate Question - 17/5/22 - Generate a random Te Reo word from a list and substitute into a question. Then generate four possible English meanings with one being correct.- V1
+# Component 2: Generate Question - 17/5/22 - Generate a random English word from a list and substitute into a question. Then generate four possible Te Reo meanings with one being correct.- V1
 score = 0
-#Lists of words based on difficulty with English counterparts
+#Lists of words based on difficulty with Te Reo counterparts
 easy_tereo = ["Kia Ora", "Kai", "Aroha", "Iwi", "Marae", "Aotearoa", "Whānau", "Mahi", "Whenua", "Taonga"]
 easy_eng = ["Greetings", "Food", "Love", "Tribe", "Meeting Ground", "New Zealand", "Family", "Work", "Land", "Treasure"]
 
-#This selects 4 random options from the list of possible Te Reo answers and one replacement answer if one of the foru options is the same as the correct one.
+#This selects 4 random options from the list of possible Te Reo answers and one replacement answer if one of the four options is the same as the correct one.
 options = random.sample(easy_tereo, 5)
 easy_A = options[0]
 easy_B = options[1]
@@ -42,16 +42,23 @@ elif easy_C == correct_word:
 elif easy_D == correct_word:
   easy_D = replacement
 
-play_again = input("Press <enter> to play.").lower()
+play_again = input("Press <enter> to play.\n").lower()
 if play_again == "":
   question_format = random.randint(1,4)
   #This randomly selects the placement of the correct answer amongst the wrong answers.
   if question_format == 1:
-      input("What is the English word for {}?\nA: {}\nB: {}\nC: {}\nD: {}\n".format(easy_word, correct_word, easy_B, easy_C, easy_D)).lower()
+     q_a_input = input("What is the English word for {}?\nA: {}\nB: {}\nC: {}\nD: {}\n".format(easy_word, correct_word, easy_B, easy_C, easy_D)).lower()
+    if q_a_input == "a":
+      print(correct)
+      
   elif question_format == 2:
-          input("What is the English word for {}?\nA: {}\nB: {}\nC: {}\nD: {}\n".format(easy_word, easy_A, correct_word, easy_C, easy_D)).lower()
+          q_b_input = input("What is the English word for {}?\nA: {}\nB: {}\nC: {}\nD: {}\n".format(easy_word, easy_A, correct_word, easy_C, easy_D)).lower()
   elif question_format == 3:
-          input("What is the English word for {}?\nA: {}\nB: {}\nC: {}\nD: {}\n".format(easy_word, easy_A, easy_B, correct_word, easy_D)).lower()
+          q_c_input = input("What is the English word for {}?\nA: {}\nB: {}\nC: {}\nD: {}\n".format(easy_word, easy_A, easy_B, correct_word, easy_D)).lower()
   else:
-          input("What is the English word for {}?\nA: {}\nB: {}\nC: {}\nD: {}\n".format(easy_word, easy_A, easy_B, easy_C, correct_word)).lower()
-    
+          q_d_input = input("What is the English word for {}?\nA: {}\nB: {}\nC: {}\nD: {}\n".format(easy_word, easy_A, easy_B, easy_C, correct_word)).lower()
+
+else:
+  print("Final Score: {}. Thank you for playing!".format(score))
+
+#Component 3: Check if user's answer is correct - 27/05/22 - Check if user's input matches correct answer and the either increase or decrease score.
