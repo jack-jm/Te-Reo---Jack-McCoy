ab25e51b7216065f0aefd80a53b0068d630587c4
Jack McCoy <101677953+jack-jm@users.noreply.github.com>
2022-05-31 01:49:47 +0000

    Started component four and think I might finish it next lesson.  Just need to sort out asking the user to play again or continue I think.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random
 show_instructions = input("Kia Ora, have you played the quiz before?\n").lower()
 
 if show_instructions == "yes" or show_instructions == "y":
-  print("Let's begin!")
+  print("Let's begin! Remember to answer with only the letter, not the word.")
 
 elif show_instructions == "no" or show_instructions == "n":
   print("Instructions:")
@@ -16,71 +16,72 @@ else:
 
 # Component 2: Generate Question - 17/5/22 - Generate a random English word from a list and substitute into a question. Then generate four possible Te Reo meanings with one being correct.- V1
 score = 0
+round_number = 0
 #Lists of words based on difficulty with Te Reo counterparts
 easy_tereo = ["Kia Ora", "Kai", "Aroha", "Iwi", "Marae", "Aotearoa", "Whānau", "Mahi", "Whenua", "Taonga"]
 easy_eng = ["Greetings", "Food", "Love", "Tribe", "Meeting Ground", "New Zealand", "Family", "Work", "Land", "Treasure"]
 
-#This selects 4 random options from the list of possible Te Reo answers and one replacement answer if one of the four options is the same as the correct one.
-options = random.sample(easy_tereo, 5)
-easy_A = options[0]
-easy_B = options[1]
-easy_C = options[2]
-easy_D = options[3]
-replacement = options[4]
+play_again = input("Press <enter> to play or type anything else to quit.\n").lower()
+while play_again == "":
+  round_number = round_number  + 1
+  #This selects 4 random options from the list of possible Te Reo answers and one replacement answer if one of the four options is the same as the correct one.
+  options = random.sample(easy_tereo, 5)
+  easy_A = options[0]
+  easy_B = options[1]
+  easy_C = options[2]
+  easy_D = options[3]
+  replacement = options[4]
 
-ans = dict(zip(easy_eng, easy_tereo))
-x = random.randint(0,9)
-easy_word = easy_eng[x]
-correct_word = easy_tereo[x]
+  ans = dict(zip(easy_eng, easy_tereo))
+  x = random.randint(0,9)
+  easy_word = easy_eng[x]
+  correct_word = easy_tereo[x]
 
-if easy_A == correct_word:
-  easy_A = replacement
-elif easy_B == correct_word:
-  easy_B = replacement
-elif easy_C == correct_word:
-  easy_C = replacement
-elif easy_D == correct_word:
-  easy_D = replacement
-
-play_again = input("Press <enter> to play.\n").lower()
-if play_again == "":
+  if easy_A == correct_word:
+    easy_A = replacement
+  elif easy_B == correct_word:
+    easy_B = replacement
+  elif easy_C == correct_word:
+    easy_C = replacement
+  elif easy_D == correct_word:
+    easy_D = replacement
   question_format = random.randint(1,4)
   #This randomly selects the placement of the correct answer amongst the wrong answers.
   if question_format == 1:
-    q_a_input = input("What is the English word for {}?\nA: {}\nB: {}\nC: {}\nD: {}\n".format(easy_word, correct_word, easy_B, easy_C, easy_D)).lower()    
+    q_a_input = input("- Round {} -\nWhat is the English word for {}?\nA: {}\nB: {}\nC: {}\nD: {}\n".format(round_number, easy_word, correct_word, easy_B, easy_C, easy_D)).lower()    
     #Component 3: Check if user's answer is correct - 27/05/22 - Check if user's input matches correct answer and the either increase or decrease score.
     if q_a_input == "a":
      score = score + 1
-     print("That is correct! Your score is {}.".format(score))
+     print("That is correct! You gained one point and your score is {}.\n".format(score))
+     play_again = input("Press <enter> to play again or type anything else to quit.\n").lower()
     else:
      score = score - 1
-     print("Sorry, that is incorrect. The correct answer was {}. Your score is {}.".format(correct_word, score))
+     print("Sorry, that is incorrect. The correct answer was {}. You lost one point and your score is {}.\n".format(correct_word, score))
+     play_again = input("Press <enter> to play again or type anything else to quit.\n").lower()
   elif question_format == 2:
-    q_b_input = input("What is the English word for {}?\nA: {}\nB: {}\nC: {}\nD: {}\n".format(easy_word, easy_A, correct_word, easy_C, easy_D)).lower()
+    q_b_input = input("- Round {} -\nWhat is the English word for {}?\nA: {}\nB: {}\nC: {}\nD: {}\n".format(round_number, easy_word, easy_A, correct_word, easy_C, easy_D)).lower()
     if q_b_input == "b":
      score = score + 1
-     print("That is correct! Your score is {}.".format(score))
+     print("That is correct! You gained one point and your score is {}.\n".format(score))
     else:
      score = score - 1
-     print("Sorry, that is incorrect. The correct answer was {}. Your score is {}.".format(correct_word, score))
+     print("Sorry, that is incorrect. The correct answer was {}. You lost one point and your score is {}.\n".format(correct_word, score))
   elif question_format == 3:
-    q_c_input = input("What is the English word for {}?\nA: {}\nB: {}\nC: {}\nD: {}\n".format(easy_word, easy_A, easy_B, correct_word, easy_D)).lower()
+    q_c_input = input("- Round {} -\nWhat is the English word for {}?\nA: {}\nB: {}\nC: {}\nD: {}\n".format(round_number, easy_word, easy_A, easy_B, correct_word, easy_D)).lower()
     if q_c_input == "c":
      score = score + 1
-     print("That is correct! Your score is {}.".format(score))
+     print("That is correct! You gained one point and your score is {}.\n".format(score))
     else:
      score = score - 1 
-     print("Sorry, that is incorrect. The correct answer was {}. Your score is {}.".format(correct_word, score))
+     print("Sorry, that is incorrect. The correct answer was {}. You lost one point and your score is {}.\n".format(correct_word, score))
   else:
-    q_d_input = input("What is the English word for {}?\nA: {}\nB: {}\nC: {}\nD: {}\n".format(easy_word, easy_A, easy_B, easy_C, correct_word)).lower()
+    q_d_input = input("- Round {} -\nWhat is the English word for {}?\nA: {}\nB: {}\nC: {}\nD: {}\n".format(round_number, easy_word, easy_A, easy_B, easy_C, correct_word)).lower()
     if q_d_input == "d":
      score = score + 1
-     print("That is correct! Your score is {}.".format(score))
+     print("That is correct! You gained one point and your score is {}.\n".format(score))
     else:
      score = score - 1
-     print("Sorry, that is incorrect. The correct answer was {}. Your score is {}.".format(correct_word, score))
+     print("Sorry, that is incorrect. The correct answer was {}. You lost one point and your score is {}.\n".format(correct_word, score))
 
 else:
   print("Final Score: {}. Thank you for playing!".format(score))
-
-#Component 3: Check if user's answer is correct - 27/05/22 - Check if user's input matches correct answer and the either increase or decrease score.
